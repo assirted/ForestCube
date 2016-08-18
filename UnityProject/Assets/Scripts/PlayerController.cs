6a89dc1b9eb2d1bae5c2e5d7893923b370d8cf75
Khairul <khairulcyber4rt@gmail.com>
2016-08-18 18:32:25 +0700

    Revert "Add mainmenu, fix game scene"
    This reverts commit d8e8254ca8b6489703b0fa7d725b31f08a2f75d3.

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
 	private float lerpTime;
 	private float currentLerpTime;
 	private float perc = 1;
-	private float moveLength = 2;
 
 	Vector3 startPos;
 	Vector3 endPos;
 
+	public bool firstInput;
 	public bool isJumping;
 	public Vector3 dir;
+	public float moveLength = 1;
 
 	void Update()
 	{
@@ -37,31 +38,31 @@ public class PlayerController : MonoBehaviour
 			dir.z -= 1;
 		}
 
-		if (gameObject.transform.position != endPos) {
-			dir = Vector3.zero;
-		}
-
 		if (dir.magnitude > 0.0 && perc == 1)
 		{
 			lerpTime = 1;
 			currentLerpTime = 0;
+			firstInput = true;
 			isJumping = true;
 		}
 
 		if (gameObject.transform.position == endPos) {
-			endPos = transform.position + dir * moveLength;
+			endPos = transform.position+dir*moveLength;
 		}
 
-		currentLerpTime += Time.deltaTime * 5;
-		perc = currentLerpTime / lerpTime;
-		gameObject.transform.position = Vector3.Lerp(startPos, endPos, perc);
-
-		if (perc > 0.8) {
-			perc = 1;
-		}
-
-		if (Mathf.Round(perc) == 1) {
-			isJumping = false;
+		if (firstInput == true)
+		{
+			currentLerpTime += Time.deltaTime * 5;
+			perc = currentLerpTime / lerpTime;
+			gameObject.transform.position = Vector3.Lerp(startPos, endPos, perc);
+			if (perc > 0.8)
+			{
+				perc = 1;
+			}
+			if (Mathf.Round(perc) == 1)
+			{
+				isJumping = false;
+			}
 		}
 	}
 }
